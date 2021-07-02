ad5fbf79f4a979c276026896c69d451823a791bb
Freddy Canido <freddy.canido12@gmail.com>
2021-07-02 13:30:11 -0400

    inputs under construction message

--- src/Java_challenges/MainMenu.java
+++ src/Java_challenges/MainMenu.java
@@ -38,7 +38,7 @@ public class MainMenu {
         System.out.println("Would you like to make inputs or use prepared code? a / b / none");
         String inputsPrepared = scan.nextLine();
         if (inputsPrepared.equalsIgnoreCase("a")) {
-            System.out.println("nothing yet");
+            System.out.println("Area under construction come back later.");
             MainMenu.main(null);
         } else if (inputsPrepared.equalsIgnoreCase("b")) {
             Display.challengeOrder();
